726cd3bbda03cbe7239b287d4e38cb7d4e32d725
developersabbir-cse <developersabbir@outlook.com>
2019-12-08 13:23:16 +0600

    problem-10

---
+++ test.py
@@ -0,0 +1,38 @@
+# x = list(map(int, input("Enter a multiple value: ").split()))
+# print(x[0])
+
+
+# x,y,z = [int(x) for x in input().split()]
+# x, y, z = int(input("Enter a three value: ")).split()
+#x = list(map(int, input("Enter a multiple value: ").split()))
+
+
+# num1 = input("Enter first number: ")
+# num2 = input("Enter second number: ")
+# if '.' in num1 and '.' in num2:
+#     num1 = float(num1)
+#     num2 = float(num2)
+# else:
+#     num2 = int(num2)
+#     num1 = int(num1)
+# print(num1+num2)
+
+
+# test_case = int(input("Enter test case: "))
+# t = 1
+# if 1 <= test_case <= 25:
+#     while test_case >= t:
+#         N = int(input("Enter number: "))
+#         n = 0
+#         if 1 <= N <= 80:
+#             while N > n:
+#                 print("*" * N)
+#                 n += 1
+#         else:
+#             print("Number should less then 81")
+#         t += 1
+# else:
+#     print("Test should be less then 25")
+# print("Finished")
+
+
